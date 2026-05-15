0e961774981509692cf8fb2f9139780366c421d1
Kishan Dobariya <flutterdeveloper2206@gmail.com>
2026-05-15 11:53:39 +0530

    Bump application version to V 1.0.7 in `GeneralUserProfilePage`.

--- lib/features/general_user/presentation/pages/general_user_profile_page.dart
+++ lib/features/general_user/presentation/pages/general_user_profile_page.dart
@@ -209,7 +209,7 @@ class _GeneralUserProfilePageState extends State<GeneralUserProfilePage> {
                                         ),
                                       ),
                                       SizedBox(height: 20,),
-                                      Text('V 1.0.6'),
+                                      Text('V 1.0.7'),
                                       const Spacer(),
                                       SizedBox(
                                         width: double.infinity,
