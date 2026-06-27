b0d6a70de85849fce5b2fa524805420efe47ba18
Kishan Dobariya <flutterdeveloper2206@gmail.com>
2026-06-27 16:17:23 +0530

    Update the version number in the profile page.
    - **UI Improvements**: Updated the version display string from `1.0.1` to `1.0.2` in `GeneralUserProfilePage`.

--- lib/features/general_user/presentation/pages/general_user_profile_page.dart
+++ lib/features/general_user/presentation/pages/general_user_profile_page.dart
@@ -713,7 +713,7 @@ class _GeneralUserProfilePageState extends State<GeneralUserProfilePage> {
 
                                         Text(
 
-                                          'Version 1.0.1',
+                                          'Version 1.0.2',
 
                                           style: Theme.of(context)
 
